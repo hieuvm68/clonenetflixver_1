444a5d1447322467335fbb392b240c6ac1befb48
hieu <hieuvm68@gmail.com>
2022-08-10 21:58:12 +0700

    Updates

---
+++ precache-manifest.edd63eeccb40d737717496e68dd24120.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e7aaeb69bbc9edf9219",
+    "url": "/clonenetflixver_1/static/css/main.76a59a0d.chunk.css"
+  },
+  {
+    "revision": "3e7aaeb69bbc9edf9219",
+    "url": "/clonenetflixver_1/static/js/main.1bda71e3.chunk.js"
+  },
+  {
+    "revision": "bd43c302c66d204d8e62",
+    "url": "/clonenetflixver_1/static/js/runtime~main.b63897b8.js"
+  },
+  {
+    "revision": "71cefa5bad195aabbaf8",
+    "url": "/clonenetflixver_1/static/js/2.5f61bf0c.chunk.js"
+  },
+  {
+    "revision": "28e9b6d94a504843191883cd8bae9247",
+    "url": "/clonenetflixver_1/index.html"
+  }
+];
